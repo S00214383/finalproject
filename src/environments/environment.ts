9ac519353158e3ac160f9cc7f431a9b7e364e466
rudgerylopes94 <63984559+rudgerylopes94@users.noreply.github.com>
2022-04-30 15:56:56 +0100

    icons and api add

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,12 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  weatherApiBaseUrl: 'https://community-open-weather-map.p.rapidapi.com/weather',
+  XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+  XRapidAPIHostHeaderValue: 'community-open-weather-map.p.rapidapi.com',
+  XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+  XRapidAPIKeyHeaderValue: '232646f06amsh4fb20b3b949523dp1dc214jsnbc6452bd7262'
 };
 
 /*
